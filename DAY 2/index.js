8d8ab80ff8a73f73233f9ef2e5bda0a26cc64b4e
mayararothe28 <mayararothe@gmail.com>
2025-07-24 07:43:12 +0530

    updates for day 2

---
+++ DAY 2/index.js
@@ -0,0 +1,38 @@
+
+// = usecase assign
+//  == usecase compare - data
+// === usecase compare strict cheking data - datatypes
+
+
+
+
+console.log(2 == 2);
+console.log(1 == 2);
+console.log("10" == 2);
+console.log(2 == 3);
+
+// === usecase compare with data types
+
+console.log("10" === 2);
+console.log("2" === 2);
+
+// graterthan operator lessorthan operator
+
+console.log(2 > 1);
+console.log(2 > 10);
+console.log(2 < 1);
+console.log(2 < 10);
+
+console.log(2 <= 2);
+console.log(20 >= 20);
+
+// and oprator
+// or oprator
+
+console.log(2 > 1 && 5 > 3 && 7 > 5 && 9 > 7);  
+// If all condition arew true then only it well return true
+// If any one of them or more than on eis false then it well return false
+
+console.log( 10 > 2 || 5 > 3);
+// if any of condition of more than is true, then it well return true.
+// if all condition of a false then it well return false.
